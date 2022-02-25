cbcdd34445b6b26307917fd5069c0ae995199662
emreozgenc <emreozgenc@hotmail.com.tr>
2022-02-25 19:22:14 +0300

    Create layers

---
+++ ExamSystem.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamSystem.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExamSystem.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamSystem.DataAccess
+{
+    public class Class1
+    {
+    }
+}
