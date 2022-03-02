056d2794b4382e4b26b0a9f0cc8ebe16fa3f02c4
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-02 15:53:10 +0300

    Add/Configure context

---
+++ ExamSystem.DataAccess/ExamSystemContext.cs
@@ -0,0 +1,42 @@
+﻿using ExamSystem.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSystem.DataAccess
+{
+    public class ExamSystemContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<StudentExam> StudentExams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite("Data Source=examsystem.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<StudentExam>()
+                .HasKey(x => new { x.StudentId, x.ExamId });
+
+            builder.Entity<StudentExam>()
+                .HasOne(x => x.Student)
+                .WithMany(x => x.StudentExams)
+                .HasForeignKey(x => x.StudentId);
+
+            builder.Entity<StudentExam>()
+                .HasOne(x => x.Exam)
+                .WithMany(x => x.StudentExams)
+                .HasForeignKey(x => x.ExamId);
+
+        }
+    }
+}
