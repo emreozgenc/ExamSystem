e17a632c32095bf4bd53dd450a02a88c57f2640c
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-02 21:53:28 +0300

    Add general repository interface

---
+++ ExamSystem.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExamSystem.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSystem.DataAccess.Abstract
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity
+    {
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        TEntity Get(int entityId);
+        void Delete(int entityId);
+        List<TEntity> GetAll();
+    }
+}
