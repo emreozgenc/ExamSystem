e6d37cf1e0607d9cf7bd22e0b9146941251f2b2a
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-02 20:55:31 +0300

    Add admin entity

---
+++ ExamSystem.Entities/Concrete/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSystem.Entities.Concrete
+{
+    public class Admin : User
+    {
+        public Admin()
+        {
+            Role = UserRole.Admin;
+        }
+    }
+}
