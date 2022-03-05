91b0a7ec2a1c50dcc1e0bc993448356d66c0aa8c
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-05 19:42:23 +0300

    Add username field and data annotations

--- ExamSystem.Entities/Concrete/User.cs
+++ ExamSystem.Entities/Concrete/User.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ExamSystem.Entities.Concrete
@@ -10,16 +11,39 @@ namespace ExamSystem.Entities.Concrete
     {
         [Key]
         public int UserId { get; set; }
+
+        [MaxLength(20)]
+        [MinLength(5)]
+        [Required]
+        public string UserName { get; set; }
+
+        [NotMapped]
+        public string Password { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
         [MaxLength(50)]
         [Required]
         public string FirstName { get; set; }
+
         [MaxLength(50)]
         [Required]
         public string LastName { get; set; }
+
+        [Required]
         public bool Banned { get; set; } = false;
+
+        [Required]
+        public bool Passive { get; set; } = true;
+
         [MaxLength(11)]
         public string Phone { get; set; }
+
+        [MaxLength(50)]
         public string Email { get; set; }
+
+        [Required]
         public UserRole Role { get; set; }
 
     }
