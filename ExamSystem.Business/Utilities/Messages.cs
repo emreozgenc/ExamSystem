255576f6e94aba9aec3468a948dfc2efbad15b75
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-06 22:43:26 +0300

    Add application messages

---
+++ ExamSystem.Business/Utilities/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSystem.Business.Utilities
+{
+    public static class Messages
+    {
+        public const string UserDoesNotExist = "Girmiş olduğunuz kullanıcı adıyla kayıtlı bir kullanıcı bulunmamaktadır.";
+        public const string WrongPassword = "Girmiş olduğunuz şifre yanlıştır.";
+        public const string UserAlreadyExists = "Girmiş olduğunuz kullanıcı adına sahip zaten bir kullanıcı bulunmaktadır.";
+        public const string NullOrEmptyUserName = "Kullanıcı adı alanı boş bırakılamaz.";
+        public const string UserNameShortOrLong = "Kullanıcı adı belirtilen sınırlar içerisinde değil.";
+        public const string UserNameContainsInvalidCharacters = "Kullanıcı adı uygunsuz karakterler içeriyor. ([a-z][A-Z][0-9])";
+        public const string NullOrEmptyPassword = "Şifre alanı boş bırakılamaz.";
+        public const string PasswordShortOrLong = "Şifre belirtilen sınırlar içerisinde değil.";
+
+    }
+}
