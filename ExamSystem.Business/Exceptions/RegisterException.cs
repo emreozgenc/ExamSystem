25de70bef77e4d32eba13c478d78c9b83df1ca82
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-06 22:45:29 +0300

    Add register exception

---
+++ ExamSystem.Business/Exceptions/RegisterException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExamSystem.Business.Exceptions
+{
+    public class RegisterException : Exception
+    {
+        public RegisterException()
+        {
+        }
+
+        public RegisterException(string message) : base(message)
+        {
+        }
+
+        public RegisterException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RegisterException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
