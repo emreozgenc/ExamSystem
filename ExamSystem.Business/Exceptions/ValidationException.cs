1268079d33f1ef925d23a83873034d3780fc163e
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-06 22:45:36 +0300

    Add validation exception

---
+++ ExamSystem.Business/Exceptions/ValidationException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExamSystem.Business.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        private IList<string> _errors;
+        public IList<string> Errors
+        {
+            get
+            {
+                return _errors;
+            }
+            private set
+            {
+                _errors = value;
+            }
+        }
+        public ValidationException(IList<string> errors)
+        {
+            _errors = errors;
+        }
+        public ValidationException()
+        {
+        }
+
+        public ValidationException(string message) : base(message)
+        {
+        }
+
+        public ValidationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
