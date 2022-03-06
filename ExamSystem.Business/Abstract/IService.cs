7752be573915fc418cd29c9bc6159009270cd393
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-06 22:44:36 +0300

    Add general service interface

---
+++ ExamSystem.Business/Abstract/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSystem.Business.Abstract
+{
+    public interface IService<TEntity>
+    {
+        void Create(TEntity entity);
+        void Delete(int entityId);
+        TEntity Get(int entityId);
+        void Update(TEntity entity);
+    }
+}
