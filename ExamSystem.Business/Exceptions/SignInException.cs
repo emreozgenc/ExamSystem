8801a36c424c1b63139b97a3f28d42cfea175ca8
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-07 17:43:07 +0300

    Add SignInException

---
+++ ExamSystem.Business/Exceptions/SignInException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExamSystem.Business.Exceptions
+{
+    public class SignInException : Exception
+    {
+        public SignInException()
+        {
+        }
+
+        public SignInException(string message) : base(message)
+        {
+        }
+
+        public SignInException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SignInException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
