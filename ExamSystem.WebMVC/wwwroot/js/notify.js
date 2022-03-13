bde4d7fb4a5b829e25edebd36ee59bb81e5ff06e
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-13 19:11:17 +0300

    Add notification scripts

---
+++ ExamSystem.WebMVC/wwwroot/js/notify.js
@@ -0,0 +1,12 @@
+﻿const notify = document.getElementById("notify");
+const notifyCloseButton = document.getElementById("notify-close");
+
+if (notify !== null && notifyCloseButton !== null) {
+    notifyCloseButton.addEventListener("click", (event) => {
+        notify.remove();
+    });
+
+    setTimeout(() => {
+        notify.remove();
+    }, notify.getAttribute("data-time"));
+}
