fc7b0f59512e8e46d08d567501495689fcf9b7c0
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-13 14:48:30 +0300

    Add general application exception

---
+++ ExamSystem.Business/Exceptions/ApplicationException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExamSystem.Business.Exceptions
+{
+    public class ApplicationException : Exception
+    {
+        protected IList<string> _errors;
+        public IList<string> Errors
+        {
+            get => _errors;
+            private set => _errors = value;
+        }
+
+        public ApplicationException()
+        {
+        }
+
+        public ApplicationException(IList<string> errors)
+        {
+            _errors = errors;
+        }
+
+        public ApplicationException(string message) : base(message)
+        {
+            _errors = new List<string>();
+            _errors.Add(message);
+        }
+
+        public ApplicationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ApplicationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
