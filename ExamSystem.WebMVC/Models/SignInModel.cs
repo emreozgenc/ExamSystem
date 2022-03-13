61c2ff4b3c312babdf926077b6b103159096cede
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-13 21:42:25 +0300

    Add SignInModel

---
+++ ExamSystem.WebMVC/Models/SignInModel.cs
@@ -0,0 +1,18 @@
+﻿using ExamSystem.Business.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamSystem.WebMVC.Models
+{
+    public class SignInModel
+    {
+        [Required(ErrorMessage = Messages.NullOrEmptyUserName)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Kullanıcı adı")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = Messages.NullOrEmptyPassword)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+    }
+}
