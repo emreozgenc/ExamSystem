63243f0a9f165e5d6673068f625ee0ca11d6d83b
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-21 21:22:24 +0300

    Add navbar collapse script

---
+++ ExamSystem.WebMVC/wwwroot/js/navbar.js
@@ -0,0 +1,9 @@
+﻿var collapseButton = document.getElementById("collapse-button");
+
+collapseButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    var targetId = collapseButton.getAttribute("data-target");
+    var target = document.getElementById(targetId);
+
+    target.classList.toggle("active");
+});
