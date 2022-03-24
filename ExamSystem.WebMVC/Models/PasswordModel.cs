0be0cd51ccf6463dd46d0fa2064e3d9ba53bb8cf
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-24 21:10:26 +0300

    Add PasswordModel for model binding

---
+++ ExamSystem.WebMVC/Models/PasswordModel.cs
@@ -0,0 +1,19 @@
+﻿using ExamSystem.Business.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamSystem.WebMVC.Models
+{
+    public class PasswordModel
+    {
+        [Required(ErrorMessage = Messages.NullOrEmptyPassword)]
+        [StringLength(12, ErrorMessage = Messages.PasswordShortOrLong, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifre tekrar alanı boş bırakılamaz.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre tekrar")]
+        public string PasswordCopy { get; set; }
+    }
+}
