52809408b705d64e2734b3c4bcd14145f3260d52
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-24 16:56:46 +0300

    Add PasswordException

---
+++ ExamSystem.Business/Exceptions/PasswordException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExamSystem.Business.Exceptions
+{
+    public class PasswordException : ApplicationException
+    {
+        public PasswordException()
+        {
+        }
+
+        public PasswordException(IList<string> errors) : base(errors)
+        {
+        }
+
+        public PasswordException(string message) : base(message)
+        {
+        }
+
+        public PasswordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected PasswordException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
