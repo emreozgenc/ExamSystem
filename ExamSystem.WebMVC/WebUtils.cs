4908f7a7ce0befd0af6ddcbd5c4746f8cd5a3361
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-24 10:50:19 +0300

    Add WebUtils and AddErrorsFromModel method

---
+++ ExamSystem.WebMVC/WebUtils.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ExamSystem.WebMVC
+{
+    public static class WebUtils
+    {
+        public static void AddErrorsFromModel(ModelStateDictionary modelStateDictionary)
+        {
+            var values = modelStateDictionary.Values;
+            foreach (ModelStateEntry modelState in values)
+            {
+                foreach (ModelError error in modelState.Errors)
+                {
+                    modelStateDictionary.AddModelError(string.Empty, error.ErrorMessage.ToString());
+                }
+            }
+        }
+    }
+}
