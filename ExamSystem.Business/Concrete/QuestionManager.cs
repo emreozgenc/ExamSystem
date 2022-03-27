dd8b0f5bc16def9c258114febe5db3b3984c0cd7
emreozgenc <emreozgenc@hotmail.com.tr>
2022-03-27 17:24:15 +0300

    Add Question services

---
+++ ExamSystem.Business/Concrete/QuestionManager.cs
@@ -0,0 +1,40 @@
+﻿using ExamSystem.Business.Abstract;
+using ExamSystem.DataAccess.Abstract;
+using ExamSystem.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSystem.Business.Concrete
+{
+    public class QuestionManager : IQuestionService
+    {
+        private IQuestionRepository _questionRepository;
+
+        public QuestionManager(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public void Create(Question entity)
+        {
+            _questionRepository.Create(entity);
+        }
+
+        public void Delete(int entityId)
+        {
+            _questionRepository.Delete(entityId);
+        }
+
+        public Question Get(int entityId)
+        {
+            var question = _questionRepository.Get(entityId);
+            return question;
+        }
+
+        public void Update(Question entity)
+        {
+            _questionRepository.Update(entity);
+        }
+    }
+}
