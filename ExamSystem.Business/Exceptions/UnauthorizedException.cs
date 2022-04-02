fd86847fa805a6457579813f99f5dbd40166661c
emreozgenc <emreozgenc@hotmail.com.tr>
2022-04-03 01:15:36 +0300

    Add UnauthorizedException

---
+++ ExamSystem.Business/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ExamSystem.Business.Exceptions
+{
+    internal class UnauthorizedException : ApplicationException
+    {
+        public UnauthorizedException()
+        {
+        }
+
+        public UnauthorizedException(IList<string> errors) : base(errors)
+        {
+        }
+
+        public UnauthorizedException(string message) : base(message)
+        {
+        }
+
+        public UnauthorizedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnauthorizedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
