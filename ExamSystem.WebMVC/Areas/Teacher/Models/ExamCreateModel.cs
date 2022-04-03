3c59ab6cf31bf3581538d7b4b980c8dfb700b81d
emreozgenc <emreozgenc@hotmail.com.tr>
2022-04-04 00:19:21 +0300

    Add ExamCreateModel (Area:Teacher)

---
+++ ExamSystem.WebMVC/Areas/Teacher/Models/ExamCreateModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamSystem.WebMVC.Areas.Teacher.Models
+{
+    public class ExamCreateModel
+    {
+        [Display(Name = "Ders adı")]
+        [Required(ErrorMessage = "Ders adı alanı boş bırakılamaz.")]
+        [DataType(DataType.Text)]
+        public string LessonName { get; set; }
+
+        [Display(Name = "Sınav seçenek sayısı")]
+        [Required(ErrorMessage = "Sınav seçenek sayısı alanı boş bırakılamaz.")]
+        [Range(2, 5, ErrorMessage = "Sınav en az iki en fazla beş şıktan oluşabilir.")]
+        public int AnswerCount { get; set; }
+
+        [Display(Name = "Başlangıç tarihi")]
+        [Required(ErrorMessage = "Başlangıç tarihi alanı boş bırakılamaz.")]
+        [DataType(DataType.DateTime)]
+        public DateTime StartTime { get; set; }
+
+        [Display(Name = "Bitiş tarihi")]
+        [Required(ErrorMessage = "Bitiş tarihi alanı boş bırakılamaz.")]
+        [DataType(DataType.DateTime)]
+        public DateTime FinishTime { get; set; }
+    }
+}
