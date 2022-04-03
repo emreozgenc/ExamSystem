9a57eebb8f77713b518e3c89faa6fa117d0dc022
emreozgenc <emreozgenc@hotmail.com.tr>
2022-04-03 23:46:59 +0300

    Add necessary Exam service methods / CRUD operations for different user types

--- ExamSystem.Business/Abstract/IExamService.cs
+++ ExamSystem.Business/Abstract/IExamService.cs
@@ -5,8 +5,14 @@ using System.Text;
 
 namespace ExamSystem.Business.Abstract
 {
-    public interface IExamService : IService<Exam>
+    public interface IExamService
     {
+        Exam GetWithQuestionsForStudent(int examId, int studentUserId);
+        Exam GetWithTeacherForStudent(int examId, int studentUserId);
+        Exam GetWithAllDetailsForTeacher(int examId, int teacherUserId);
+        void Delete(int examId, int teacherUserId);
+        void Update(Exam exam, int teacherUserId);
+        void Create(Exam exam);
 
     }
 }
