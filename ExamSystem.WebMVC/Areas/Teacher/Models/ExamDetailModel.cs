3b8fcdf4d099ddcfa993ee88a58c69d30754c4ca
emreozgenc <emreozgenc@hotmail.com.tr>
2022-04-04 00:18:53 +0300

    Add ExamDetailModel (Area:Teacher)

---
+++ ExamSystem.WebMVC/Areas/Teacher/Models/ExamDetailModel.cs
@@ -0,0 +1,9 @@
+﻿using ExamSystem.Entities.Concrete;
+
+namespace ExamSystem.WebMVC.Areas.Teacher.Models
+{
+    public class ExamDetailModel
+    {
+        public Exam Exam { get; set; }
+    }
+}
