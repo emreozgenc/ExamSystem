bc9742a3ea198c353b3a1fa352a8714ec1e66f02
emreozgenc <emreozgenc@hotmail.com.tr>
2022-04-03 23:48:19 +0300

    Implement IExamService interface and Authorized methods for different user types

--- ExamSystem.Business/Concrete/ExamManager.cs
+++ ExamSystem.Business/Concrete/ExamManager.cs
@@ -1,4 +1,6 @@
 ﻿using ExamSystem.Business.Abstract;
+using ExamSystem.Business.Exceptions;
+using ExamSystem.Business.Utilities;
 using ExamSystem.DataAccess.Abstract;
 using ExamSystem.Entities.Concrete;
 using System;
@@ -16,24 +18,70 @@ namespace ExamSystem.Business.Concrete
             _examRepository = examRepository;
         }
 
+        public Exam GetWithAllDetailsForTeacher(int examId, int teacherUserId)
+        {
+            CheckIfTeacherHasExam(examId, teacherUserId);
+
+            var exam = _examRepository.GetWithAllDetails(examId);
+
+            return exam;
+        }
+
+        private void CheckIfTeacherHasExam(int examId, int teacherUserId)
+        {
+            var result = _examRepository.TeacherHasExam(examId, teacherUserId);
+
+            if(!result)
+            {
+                throw new UnauthorizedException(Messages.TeacherDoesNotHaveTheExam);
+            }
+        }
+
+        public Exam GetWithQuestionsForStudent(int examId, int studentUserId)
+        {
+            CheckIfStudentHasExam(examId, studentUserId);
+
+            var exam = _examRepository.GetWithQuestions(examId);
+
+            return exam;
+        }
+
+        public Exam GetWithTeacherForStudent(int examId, int studentUserId)
+        {
+            CheckIfStudentHasExam(examId, studentUserId);
+
+            var exam = _examRepository.GetWithTeacher(examId);
+
+            return exam;
+        }
+
+        private void CheckIfStudentHasExam(int examId, int studentUserId)
+        {
+            var result = _examRepository.StudentHasExam(examId, studentUserId);
+
+            if (!result)
+            {
+                throw new UnauthorizedException(Messages.StudentDoesNotHaveTheExam);
+            }
+        }
+
+        public void Delete(int examId, int teacherUserId)
+        {
+            CheckIfTeacherHasExam(examId, teacherUserId);
+
+            _examRepository.Delete(examId);
+        }
+
+        public void Update(Exam exam, int teacherUserId)
+        {
+            CheckIfTeacherHasExam(exam.ExamId, teacherUserId);
+
+            _examRepository.Update(exam);
+        }
+
         public void Create(Exam exam)
         {
             _examRepository.Create(exam);
-        }
-
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Exam Get(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Exam exam)
-        {
-            throw new NotImplementedException();
         }
     }
 }
